6fc7b53ae14f141d9021acfc061cdb630f66fe15
Tanmay Gupta <tanmay@Tanmays-MacBook-Air.local>
2026-06-29 21:11:35 +0530

    feat: complete Flights Service

---
+++ Flights-Service/src/middlewares/airplane-middleware.js
@@ -0,0 +1,13 @@
+const { StatusCodes } = require("http-status-codes");
+const { ErrorResponse } = require("../utils/common");
+
+function validateCreateRequest(req, res, next) {
+  if (!req.body.modelNumber) {
+    ErrorResponse.message = "Something went wrong while creating airplane";
+    ErrorResponse.explanation = "Model number not found in the request body";
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  next();
+}
+
+module.exports = { validateCreateRequest };
