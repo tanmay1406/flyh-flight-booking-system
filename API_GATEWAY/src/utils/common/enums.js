9d5c9b7014f6aa4a762b767acc7351081db88cd0
Tanmay Gupta <tanmay@Tanmays-MacBook-Air.local>
2026-06-29 21:07:54 +0530

    feat: add gateway utilities

---
+++ API_GATEWAY/src/utils/common/enums.js
@@ -0,0 +1,21 @@
+const SEAT_TYPE = {
+  BUISNESS: "buisness",
+  ECONOMY: "economy",
+  PREMIUM_ECONOMY: "premium_economy",
+  FIRST_CLASS: "first_class",
+};
+
+const BOOKING_STATUS = {
+  PENDING: "pending",
+  BOOKED: "booked",
+  CANCELLED: "cancelled",
+  INITIATED: "initiated",
+};
+
+const ROLE = {
+  ADMIN: "admin",
+  CUSTOMER: "customer",
+  FLIGHT_COMPANY: "flight_company",
+};
+
+module.exports = { SEAT_TYPE, BOOKING_STATUS, ROLE };
